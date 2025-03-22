6afc720dddb25cf91daec4a6b94dce2251d8c295
SinlessDevil <83553523+SinlessDevil@users.noreply.github.com>
2025-03-22 16:11:57 +0200

    Added congifs

--- Assets/Scripts/StaticData/Heroes/HeroTypeId.cs
+++ Assets/Scripts/StaticData/Heroes/HeroTypeId.cs
@@ -1,11 +1,31 @@
 ﻿namespace CodeBase.StaticData.Heroes
 {
-  public enum HeroTypeId
-  {
-    Unknown = 0,
-    Angel = 1,
-    FireMage = 2,
-    Monk = 3,
-    Reaper = 4,
-  }
+    public enum HeroTypeId
+    {
+        Unknown = 0,
+        Bat = 1,
+        BatLord = 2,
+        Bee = 3,
+        Bird = 4,
+        Chick = 5,
+        Egg = 6,
+        Fledgling = 7,
+        Flower = 8,
+        Fungi = 9,
+        Ghost = 10,
+        Mushroom = 11,
+        Phantom = 12,
+        Seed = 13,
+        Shade = 14,
+        Shadow = 15,
+        Spider = 16,
+        SpiderKing = 17,
+        SpiderToxin = 18,
+        Spook = 19,
+        Sproul = 20,
+        Sting = 21,
+        Toadstool = 22,
+        Vampire = 23,
+        StingLord = 24,
+    }
 }
